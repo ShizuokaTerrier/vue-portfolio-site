ee6e92bbfc3aeff7b66f5db11c01384685021c34
ShizuokaTerrier <matt@squareoneJapan.com>
2024-10-08 22:10:35 +0900

    fix: remove padding to fix responsive layout

--- vue-portfolio-matt/src/components/AboutMeGreeting.vue
+++ vue-portfolio-matt/src/components/AboutMeGreeting.vue
@@ -6,11 +6,11 @@ import heroIMG from '../assets/matt-keighley.png';
   <div class="flex flex-col md:flex-row items-center justify-center h-screen">
     <img :src="heroIMG" alt="" />
     <div class="pl-6">
-      <h2 class="py-4 px-4">
+      <h2 class="py-4">
         Hi, I'm <span class="font-bold">Matt Keighley</span><br />
         A <span class="font-bold">fullstack software engineer</span>.
       </h2>
-      <h4 class="italic px-4">
+      <h4 class="italic">
         Born in America, raised in the UK, seasoned in Japan.
       </h4>
     </div>
